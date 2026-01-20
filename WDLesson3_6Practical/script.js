8a98b3837dd84c489ea83ac693e343544676e586
aiden456789 <aidena78@nycstudents.net>
2026-01-20 14:32:15 +0000

    update 3.6

--- WDLesson3_6Practical/script.js
+++ WDLesson3_6Practical/script.js
@@ -1,4 +1,6 @@
 // Challenge 2:  Create the function for the Average program.  Refer to the resource link in the HTML for assistance.
+ avg=(g1+g2+g3)/3;
+
 
 
 // Challenge 4:  Create the function for the Slope program.  Refer to the resource link in the HTML for assistance.
