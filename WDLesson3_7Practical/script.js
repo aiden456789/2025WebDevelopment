66e2878835f140a2a931df3e108c5a6ef9e2d8c5
aiden456789 <aidena78@nycstudents.net>
2026-01-22 14:31:03 +0000

    update 3.7

--- WDLesson3_7Practical/script.js
+++ WDLesson3_7Practical/script.js
@@ -3,7 +3,12 @@
       2) Perform the necessary calculations
       3) Display the results in the appropriate element
 */
+function() Distance{
+     let p1 = document.getElementById("Point 1");
+     let p2 = document.getElementById("Point 2");
 
+     outout.innerHTML = distance;
+}
 /* Challenge 4: Create a function to serve as the event handler for the compound interest UI. Guidelines,
       1) Create variables and retrieve the information from the text inputs you created in Challenge 1.
       2) Perform the necessary calculations
