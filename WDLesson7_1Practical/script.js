f0bef1a6c4a5dacf6b2d86436d88d05d55df55b2
aiden456789 <aidena78@nycstudents.net>
2026-05-05 13:32:50 +0000

    update 7.1

---
+++ WDLesson7_1Practical/script.js
@@ -0,0 +1,35 @@
+
+function init(){
+  //For all challenges use the function display( ) in order to output the results to the console and the webpage.  Below is an example
+  let secret = rnd(1,10);
+  display(secret);
+  
+  //Challenge 1: Use the function area( ) created in helper.js to display the area of any rectangle of your choice.
+function area( length, width){
+  return length * width;
+}
+
+  //Challenge 2: Use the function celsius(  ) created in helper.js to celsius equivalent of any fahrenheit temperature. 
+function celsius( temp ){
+  return (temp - 32) * 5 / 9;
+}
+
+  //Challenge 4: Set height and weight to appropriate numbers. Pass the variables as arguments to the bmi() created in Challenge 3 and store in a variable called b. Display the value of b.
+  let height = 68 ; // in inches
+  let weight = 145; // in pounds
+  let b = BMI(weight, height);
+
+  //Challenge 6:  Passed the value b from the previous challenge to the bmiCategory() and stored in a variable bc.  Display the category returned by bmiCategory().
+ function displayBMI(){
+
+  
+  //Challenge 8: Call lottery with some number and display the results.
+
+
+
+
+  //Challengen 10: Create an array, survey which contains several of "Y" and "N" response.  Pass the survey to the results( ) and display the value returned.
+
+  
+}
+
